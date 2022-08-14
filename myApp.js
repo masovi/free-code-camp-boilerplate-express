b2f22fee9b01df656a933d9c08810c322985649d
solmar <solmar@dexeus.com>
2022-08-14 09:36:20 +0200

    serve an HMTL file

--- myApp.js
+++ myApp.js
@@ -3,8 +3,8 @@ let app = express();
 
 console.log("Hello Express");
 
-app.get("", function (req, res) {
-    res.send("Hello Express");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
 });
 
 
