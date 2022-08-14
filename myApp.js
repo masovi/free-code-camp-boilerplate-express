33703866837da44b3c12e79baecfd158d7433b01
solmar <solmar@dexeus.com>
2022-08-14 10:08:13 +0200

    serve static assets

--- myApp.js
+++ myApp.js
@@ -3,38 +3,10 @@ let app = express();
 
 console.log("Hello Express");
 
+app.use("/public", express.static(__dirname + '/public'));
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = app;
