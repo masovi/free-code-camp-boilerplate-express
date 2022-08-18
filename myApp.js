767aa807873914b256776d5a90f99f4f29050180
solmar <solmar@dexeus.com>
2022-08-18 09:43:25 +0200

    get route parameter input from the client

--- myApp.js
+++ myApp.js
@@ -33,4 +33,8 @@ app.get("/now", function (req, res, next) {
     res.send({ "time": req.time });
 });
 
+app.get("/:word/echo", function (req, res) {
+    res.send({ "echo": req.params.word });
+});
+
 module.exports = app;
