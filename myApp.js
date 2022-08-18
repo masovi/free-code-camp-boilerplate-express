2d15d6bbd47a73d69c4ccfbd3566b5adec49c0db
solmar <solmar@dexeus.com>
2022-08-18 09:57:42 +0200

    get query parameter input from the client

--- myApp.js
+++ myApp.js
@@ -37,4 +37,9 @@ app.get("/:word/echo", function (req, res) {
     res.send({ "echo": req.params.word });
 });
 
+app.get("/name", function (req, res) {
+    const name = req.query.firstname + ' ' + req.query.lastname;
+    res.send({ "name": name });
+});
+
 module.exports = app;
